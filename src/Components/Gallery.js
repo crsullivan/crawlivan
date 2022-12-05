92d6db6111c770ae410dd888fa2020cd5c77c7d0
Connor Sullivan <crsullivan@protonmail.com>
2022-12-05 11:19:57 -0700

    Adds and formats gallery images

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -4,7 +4,16 @@ import './Gallery.css';
 function Gallery() {
     return (
         <div class="gallery-container">
-            <p>Gallery</p>
+            <img src={require("../Assets/_DSC8777.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img src={require("../Assets/_DSC8795.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img src={require("../Assets/_DSC9500.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img class="wide-image" src={require("../Assets/_DSC8882.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img src={require("../Assets/_DSC9521.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img src={require("../Assets/_DSC9835.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img src={require("../Assets/_DSC8893.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img class="wide-image" src={require("../Assets/_DSC8915.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+          
+            {/* <img src={require("../Assets/_DSC9905.jpg")} alt='Banner with the names of the brdie and groom.'></img> */}
         </div>
     )
   }
