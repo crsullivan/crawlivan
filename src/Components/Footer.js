9802e06e6e6ee0fb7762cbf61508bfc96716bcd1
Connor Sullivan <crsullivan@protonmail.com>
2022-12-05 11:24:08 -0700

    Replaces local links with web links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
 
                 </p>
                 <div class="footernavitem">
-                    <a href="http://localhost:3000/contact"  class="navlink">Contact Us</a>
+                    <a href="https://crawlivan.wedding/contact"  class="navlink">Contact Us</a>
                 </div>
             </div>    
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,16 +8,16 @@ function Header() {
             <img src={require("../Assets/Banner.png")} alt='Banner with the names of the brdie and groom.'></img>
             <div class="navs">
                 <div class="navitem">
-                    <a href="http://localhost:3000/" class="navlink">Home</a>
+                    <a href="https://crawlivan.wedding/" class="navlink">Home</a>
                 </div>
                 <div class="navitem">
-                    <a href="http://localhost:3000/bridal-party" class="navlink">Bridal Party</a>
+                    <a href="https://crawlivan.wedding/bridal-party" class="navlink">Bridal Party</a>
                 </div>
                 <div class="navitem">
-                    <a href="http://localhost:3000/venue" class="navlink">Venue</a>
+                    <a href="https://crawlivan.wedding/venue" class="navlink">Venue</a>
                 </div>
                 <div class="navitem">
-                    <a href="http://localhost:3000/gallery" class="navlink">Gallery</a>
+                    <a href="https://crawlivan.wedding/gallery" class="navlink">Gallery</a>
                 </div>
             </div>
         </div>
