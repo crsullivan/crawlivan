229bb823983ab75c192c93a1d4fe0f33a3ac29fc
Connor Sullivan <crsullivan@protonmail.com>
2022-12-06 16:47:38 -0700

    Locks down image downloads

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './Gallery.css';
 
 function Gallery() {
+    let allImages = document.querySelectorAll("img");
+    allImages.forEach((value)=>{
+        value.oncontextmenu = (e)=>{
+            e.preventDefault();
+        }
+    })
     return (
         <div className="gallery-container">
-            <img src={require("../Assets/_DSC8777.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img onContextMenu="return false;" src={require("../Assets/_DSC8777.jpg")} alt='Banner with the names of the brdie and groom.'></img>
             <img src={require("../Assets/_DSC8795.jpg")} alt='Banner with the names of the brdie and groom.'></img>
             <img src={require("../Assets/_DSC9500.jpg")} alt='Banner with the names of the brdie and groom.'></img>
             <img className="wide-image" src={require("../Assets/_DSC8882.jpg")} alt='Banner with the names of the brdie and groom.'></img>
@@ -12,8 +18,6 @@ function Gallery() {
             <img src={require("../Assets/_DSC9835.jpg")} alt='Banner with the names of the brdie and groom.'></img>
             <img src={require("../Assets/_DSC8893.jpg")} alt='Banner with the names of the brdie and groom.'></img>
             <img className="wide-image" src={require("../Assets/_DSC8915.jpg")} alt='Banner with the names of the brdie and groom.'></img>
-          
-            {/* <img src={require("../Assets/_DSC9905.jpg")} alt='Banner with the names of the brdie and groom.'></img> */}
         </div>
     )
   }
