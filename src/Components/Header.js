99dff0a46f85f849381e72e72b9bdca285e51c20
Connor Sullivan <crsullivan@protonmail.com>
2022-12-06 08:35:16 -0700

    Conditional rendering of Navs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,19 +5,19 @@ import '../Assets/Banner.png'
 function Header() {
     return (
         <div class="header">
-            <img src={require("../Assets/Banner.png")} alt='Banner with the names of the brdie and groom.'></img>
+            <img src={require("../Assets/Banner.png")} alt='Banner with the names of the bride and groom.'></img>
             <div class="navs">
                 <div class="navitem">
-                    <a href="https://crawlivan.wedding/" class="navlink">Home</a>
+                    <a href="https://crawlivan.wedding/" style={{color: window.location.href==='http://localhost:3000/' ? 'turquoise' : 'return'}} class="navlink">Home</a>
                 </div>
                 <div class="navitem">
-                    <a href="https://crawlivan.wedding/bridal-party" class="navlink">Bridal Party</a>
+                    <a href="https://crawlivan.wedding/bridal-party" style={{color: window.location.href==='http://localhost:3000/bridal-party' ? 'turquoise' : 'return'}} class="navlink">Bridal Party</a>
                 </div>
                 <div class="navitem">
-                    <a href="https://crawlivan.wedding/venue" class="navlink">Venue</a>
+                    <a href="https://crawlivan.wedding/venue" style={{color: window.location.href==='http://localhost:3000/venue' ? 'turquoise' : 'return'}} class="navlink">Venue</a>
                 </div>
                 <div class="navitem">
-                    <a href="https://crawlivan.wedding/gallery" class="navlink">Gallery</a>
+                    <a href="https://crawlivan.wedding/gallery" style={{color: window.location.href==='http://localhost:3000/gallery' ? 'turquoise' : 'return' }} class="navlink">Gallery</a>
                 </div>
             </div>
         </div>
