b0030432a92eb3d9a1c22c884af63301dc52a023
Connor Sullivan <crsullivan@protonmail.com>
2022-12-06 16:28:11 -0700

    Format FAQ

--- src/Components/FAQ.js
+++ src/Components/FAQ.js
@@ -3,8 +3,38 @@ import './FAQ.css';
 
 function FAQ() {
     return (
-        <div className="container">
-            <p>FAQ</p>
+        <div className="faq-container">
+            <h2>Frequently Asked Questions</h2>
+            <div className='faq-content-boxes'>
+                <h3 className='faq-questions'>Question 1</h3>
+                <div className='faq-answers'>
+                    <p className='answer'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+            <div className='faq-content-boxes'>
+                <h3 className='faq-questions'>Question 2</h3>
+                <div className='faq-answers'>
+                    <p className='answer'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+            <div className='faq-content-boxes'>
+                <h3 className='faq-questions'>Question 3</h3>
+                <div className='faq-answers'>
+                    <p className='answer'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+            <div className='faq-content-boxes'>
+                <h3 className='faq-questions'>Question 4</h3>
+                <div className='faq-answers'>
+                    <p className='answer'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+            <div className='faq-content-boxes'>
+                <h3 className='faq-questions'>Question 5</h3>
+                <div className='faq-answers'>
+                    <p className='answer'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
         </div>
     )
   }
