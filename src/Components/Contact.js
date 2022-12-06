b2014a568026f1399aeaf3e548cdfc3710cad79b
Connor Sullivan <crsullivan@protonmail.com>
2022-12-06 11:17:10 -0700

    Format contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -18,18 +18,30 @@ function Contact() {
     return (
         <div class="contact-container">
             <form className="contact-form" onSubmit={sendEmail}>
-                <input type="hidden" name="contact_number" />
-                <label>Name</label>
-                <input type="text" name="from_name" />
-                <label>Email</label>
-                <input type="email" name="from_email" />
-                <label>Phone Number</label>
-                <input type="number" name="phone" />
-                <label>Guest Count</label>
-                <input type="text" name="count" />
-                <label>Message</label>
-                <textarea name="html_message" />
-                <input type="submit" value="Send" />
+                    <input type="hidden" name="contact_number" />
+                <div class="input-container">
+                    <label>First and Last Name: </label>
+                    <input type="text" name="from_name" />
+                </div>
+                <div class="input-container">
+                    <label>Email Address: </label>
+                    <input type="email" name="from_email" />
+                </div>
+                <div class="input-container">
+                    <label>Phone Number: </label>
+                    <input type="number" name="phone" />
+                </div>
+                <div class="input-container">
+                    <label>Guest Count: </label>
+                    <input type="text" name="count" />
+                </div>
+                <div class="input-container">
+                    <label>Message: </label>
+                    <textarea name="html_message" />
+                </div>
+                <div class="input-container">
+                    <input type="submit" value="Send" />
+                </div>
             </form>
         </div>
     )
