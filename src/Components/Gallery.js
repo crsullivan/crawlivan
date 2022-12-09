fb44a6b0eec8194c3299d83598b43714c30386f0
Connor Sullivan <crsullivan@protonmail.com>
2022-12-09 11:00:05 -0700

    Locks down drag and drop from gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -7,14 +7,14 @@ function Gallery() {
 
     return (
         <div className="gallery-container">
-            <img onContextMenu="return false;" src={require("../Assets/_DSC8777.jpg")} alt='Banner with the names of the brdie and groom.'></img>
-            <img src={require("../Assets/_DSC8795.jpg")} alt='Banner with the names of the brdie and groom.'></img>
-            <img src={require("../Assets/_DSC9500.jpg")} alt='Banner with the names of the brdie and groom.'></img>
-            <img className="wide-image" src={require("../Assets/_DSC8882.jpg")} alt='Banner with the names of the brdie and groom.'></img>
-            <img src={require("../Assets/_DSC9521.jpg")} alt='Banner with the names of the brdie and groom.'></img>
-            <img src={require("../Assets/_DSC9835.jpg")} alt='Banner with the names of the brdie and groom.'></img>
-            <img src={require("../Assets/_DSC8893.jpg")} alt='Banner with the names of the brdie and groom.'></img>
-            <img className="wide-image" src={require("../Assets/_DSC8915.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img draggable={false} src={require("../Assets/_DSC8777.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img draggable={false} src={require("../Assets/_DSC8795.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img draggable={false} src={require("../Assets/_DSC9500.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img draggable={false} className="wide-image" src={require("../Assets/_DSC8882.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img draggable={false} src={require("../Assets/_DSC9521.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img draggable={false} src={require("../Assets/_DSC9835.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img draggable={false} src={require("../Assets/_DSC8893.jpg")} alt='Banner with the names of the brdie and groom.'></img>
+            <img draggable={false} className="wide-image" src={require("../Assets/_DSC8915.jpg")} alt='Banner with the names of the brdie and groom.'></img>
         </div>
     )
   }
